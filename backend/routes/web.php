10e8cec80ece82c8ce891e5be3877fa7852a33c4
muhammadderic <muhammad.donny.e@gmail.com>
2025-04-08 11:51:12 +0700

    Create initial auth feature and show login and register page

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+  return view('home');
+})
+  ->middleware('auth')
+  ->name('home');
+
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
